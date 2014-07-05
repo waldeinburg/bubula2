1c66ea255b419bd0d649ece2f20056cc7928dd2f
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2014-07-06 00:13:18 +0200

    Move to standard fabric but keep using fabconfig. Breaks all fab commands.

--- fabfile.py
+++ fabfile.py
@@ -1,90 +1,83 @@
+import sys
 from os import chdir
+from os.path import exists
 from datetime import datetime
 import time
+import yaml
 from fabric.api import env, task, local, run, cd, abort
 from fabric.colors import yellow, red
+from fabric.utils import _AttributeDict
 from django.core import management
 
 env.colors = True
 env.format = True
 env.config_file = 'fabconfig.yaml'
-env.config_fileTpl = 'fabconfig.tpl.yaml'
-env.privateDataFile = 'private'
-env.settingsTpl = 'settings.tpl.py'
-env.settingsDir = 'settings'
+env.config_file_tpl = 'fabconfig.tpl.yaml'
+env.private_data_file = 'private'
+env.settings_tpl = 'settings.tpl.py'
+env.settings_dir = 'settings'
 env.releaseTS = int(round( time.time() ))
 env.release = datetime.fromtimestamp(env.releaseTS).strftime('%y%m%d%H%M%S')
 env.optimizeImages = False
 env.yuicompressorPath = '/usr/local/bin/yuicompressor-2.4.7.jar'
 
+def _build_fab_conf():
+    local('./simple-proc-tpl.sh {config_file_tpl} {private_data_file} {config_file}'.format(**env))
+
+
+def _setup():
+    # Load config file
+    if not exists(env.config_file):
+        print yellow('>>> fabconfig.yaml not found. Attempt to build from template ...')
+        _build_fab_conf()
+    config_file = open(env.config_file, 'rb')
+    env.config = _AttributeDict(yaml.safe_load(config_file.read()))
+    config_file.close()
+
+    # Set hosts from config file
+    env.hosts = env.config.hosts
+
+    # Process paths, allowing references
+    process_again = True
+    while process_again:
+        process_again = False
+        for p in env.config.paths:
+            env.config.paths[p] = env.config.paths[p].format(**env.config.paths)
+            if env.config.paths[p].find('{') != -1:
+                process_again = True
+_setup()
+
+
 # This is taken from
 # https://github.com/tkopczuk/one_second_django_deployment/blob/master/fabfile.py
 # Removed S3 and gzipping and adopted to coding style
 @task
-def buildstatic():
+def build_static():
     print yellow('>>> collecting static')
     local('echo yes | env python manage.py collectstatic', capture=False)
     
     print yellow('>>> building static')
     def get_file_base_and_extension(filename):
         if filename.count('.'):
-            return '.'.join( filename.split('.')[:-1] ), '.{0}'.format( filename.split('.')[-1] )
+            return '.'.join(filename.split('.')[:-1]), '.{0}'.format(filename.split('.')[-1])
         else:
             return filename, ''
     
     # bundle
-    for bundlePath in env.config.default['bundle_and_minify']:
-        with open(bundlePath, 'w') as outfile:
-            for libpath in env.config.default['bundle_and_minify'][bundlePath]:
+    for bundle_path in env.config.bundle_and_minify:
+        with open(bundle_path, 'w') as outfile:
+            for libpath in env.config.bundle_and_minify[bundle_path]:
                 with open(libpath, 'r') as libfile:
                     outfile.write( libfile.read() )
         # this replaces the large code block below for our purposes
-        outpathComponents = get_file_base_and_extension(bundlePath)
+        outpathComponents = get_file_base_and_extension(bundle_path)
         minifiedPath = '{0}.min{1}'.format(outpathComponents[0], outpathComponents[1])
-        local('java -jar {yuicompressor} -v -o "{outFile}" --charset utf-8 "{inFile}"'.format(yuicompressor=env.yuicompressorPath, outFile=minifiedPath, inFile=bundlePath), capture=False )
-
-        # The following from Ask The Pony will minify all js and css in static.
-        # But what is the point of this without altering apps to include them?
-        # Outcomment for now.
-#    interestingExtensions = set(['.js', '.css'])
-#    if env.optimizeImages:
-#        interestingExtensions.update(set(['.png', '.jpg', '.jpeg']))
-#
-#    # get list of files to handle
-#    dirList = list( os.walk(env.config.default['staticRelativeDir']) )
-#    filesSet = set()
-#    for directory in dirList:
-#        filesSet.update(set([
-#            get_file_base_and_extension( os.path.join(directory[0], d) )
-#                for d in directory[2]
-#                    if get_file_base_and_extension(d)[1] in interestingExtensions
-#                        and not get_file_base_and_extension(d)[0][-4:] == '.min' # .count('.min') is inaccurate. This method does not yield errors on short strings.
-#        ]))
-#
-#    # compress files
-#    filesNo = len(filesSet)
-#    for index, (fileBasename, fileExtension) in enumerate(filesSet):
-#        print green( '{part:.1%} done ({index}/{filesNo})'.format( part=(index+1.)/filesNo, index=index+1, filesNo=filesNo) )
-#        originalPath = '{0}{1}'.format(fileBasename, fileExtension)
-#        minifiedPath = '{0}.min{1}'.format(fileBasename, fileExtension)
-#
-#        if fileExtension in ['.png']:
-#            local( 'pngcrush -rem gAMA -rem cHRM -rem iCCP -rem sRGB -rem alla -rem text -reduce -brute "{0}" "{1}"'.format(originalPath, minifiedPath) )
-#            try:
-#                print yellow( '\tcompressed {0}'.format(os.path.getsize(originalPath)) + ' => {0}'.format(os.path.getsize(minifiedPath)) + " => {0:.0%}".format(os.path.getsize(minifiedPath) / os.path.getsize(originalPath)) )
-#            except OSError:
-#                print red( 'File {0} is not a PNG file.'.format(originalPath) )
-#        elif fileExtension in ['.jpg', '.jpeg']:
-#            local( 'jpegtran -outfile "{outFile}" -optimize -copy none "{inFile}"'.format(outFile=minifiedPath, inFile=originalPath) )
-#            try:
-#                print yellow( '\tcompressed {0}'.format(os.path.getsize(originalPath)) + ' => {0}'.format(os.path.getsize(minifiedPath)) + " => {0:.0%}" % (os.path.getsize(minifiedPath) / os.path.getsize(originalPath)) )
-#            except OSError:
-#                print red( 'File {0} is not a JPEG file.'.format(originalPath) )
-#        elif fileExtension in ['.js', '.css']:
-#            local('java -jar {yuicompressor} -v -o "{outFile}" --charset utf-8 "{inFile}"'.format(yuicompressor=env.yuicompressorPath, outFile=minifiedPath, inFile=originalPath) )
+        local('java -jar {yuicompressor} -v -o "{outFile}" --charset utf-8 "{inFile}"'.format(
+                yuicompressor=env.yuicompressorPath, outFile=minifiedPath, inFile=bundle_path),
+              capture=False)
 
 
-@task('app-servers')
+@task#('app-servers')
 def deploy():
     print yellow('>>> generating dummy config-file for the public')
     # hack
@@ -109,10 +102,10 @@ def _deploy():
     print yellow('>>> syncing remote settings')
     local( 'rsync -avuz settings/settings-{host_string}.py {user}@{host_string}:{path}/{project}/{project}/settings.py'.format(**env) )
     print yellow('>>> syncing static')
-    local( 'rsync -avuz --delete {staticRelativeDir}/ {user}@{host_string}:{static}/'.format(**env), capture=False )
+    local( 'rsync -avuz --delete {static_relative_dir}/ {user}@{host_string}:{static}/'.format(**env), capture=False )
 
 
-@task('app-servers')
+@task#('app-servers')
 def restart():
     env().multirun(_restart)
 
@@ -124,8 +117,8 @@ def _restart():
 
 
 @task
-def recollectstatic():
-    with cd(env.config.default['staticRelativeDir']):
+def recollect_static():
+    with cd(env.config.default['static_relative_dir']):
         print yellow('>>> removing all files in static')
         local('rm -r *')
     print yellow('>>> collecting static')
@@ -135,8 +128,8 @@ def recollectstatic():
     local('echo yes | python manage.py collectstatic')
 
 
-@task('app-servers')
-def remotecollectstatic():
+@task#('app-servers')
+def remote_collect_static():
     env().multirun(_remote_collect_static)
 
 
@@ -148,7 +141,7 @@ def _remote_collect_static():
 
 
 @task
-def buildtranslation():
+def build_translation():
     # http://blog.brendel.com/2010/09/how-to-customize-djangos-default.html
     chdir(env.config.default['project'])
     print yellow('>>> translating all messages')
@@ -159,20 +152,27 @@ def buildtranslation():
     management.call_command('compilemessages')
 
 
-@task('app-servers')
+@task#('app-servers')
 def buildsettings():
     env().multirun(_build_settings)
 
 
 def _build_settings():
     print yellow('>>> building settings file')
-    local('./simple-proc-tpl.sh {settingsTpl} {privateDataFile} {settingsDir}/settings-{host_string}.py'.format(**env))
+    local('./simple-proc-tpl.sh {settings_tpl} {private_data_file} {settings_dir}/settings-{host_string}.py'.format(**env))
 
 
 @task
-def rebuildconf():
-    # The first time, first run $ touch fabconfig.yaml
+def rebuild_fab_conf():
     # The file private contains lines of the form
     # VARIABLE=value
     print yellow('>>> rebuilding {config_file}'.format(**env))
-    local('./simple-proc-tpl.sh {config_fileTpl} {privateDataFile} {config_file}'.format(**env))
+    _build_fab_conf()
+
+
+@task
+def local_recreate_env():
+    print yellow('>>> deleting env')
+    # delete env (silent fail)
+    # local:
+    #   pip install -r envreq.txt --allow-external PIL --allow-unverified PIL
