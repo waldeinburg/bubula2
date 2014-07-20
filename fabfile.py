37d2c2fd9447294c90f84db8d0eadaf817920bf9
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2014-07-20 22:39:40 +0200

    Some parts of test deployment done.

--- fabfile.py
+++ fabfile.py
@@ -4,14 +4,17 @@ from os.path import exists
 from datetime import datetime
 import time
 import yaml
-from fabric.api import env, task, local, run, abort
-from fabric.context_managers import cd, lcd
+from fabric.api import env, task, local, run, abort, cd, lcd
 from fabric.colors import yellow, red
 from fabric.utils import _AttributeDict
+from fabric.contrib import files
 from django.core import management
 
-env.colors = True
-env.format = True
+# Fabric native settings
+env.use_ssh_config = True
+env.colorize_errors = True
+
+# Other
 env.config_file = 'fabconfig.yaml'
 env.config_file_tpl = 'fabconfig.tpl.yaml'
 env.private_data_dir = 'private'
@@ -38,7 +41,7 @@ def _setup():
     config_file.close()
 
     # Set hosts from config file
-    env.hosts = env.config.hosts
+    env.hosts = [ env.config.host ]
 
     # Process paths, allowing references
     paths = _AttributeDict(env.config.paths)
@@ -58,7 +61,7 @@ _setup()
 # Removed S3 and gzipping and adopted to coding style
 @task
 def build_static():
-    recollect_static()
+    local_recollect_static()
     
     _msg('building static')
     def get_file_base_and_extension(filename):
@@ -73,16 +76,53 @@ def build_static():
             for libpath in env.config.bundle_and_minify[bundle_path]:
                 with open(libpath, 'r') as libfile:
                     outfile.write(libfile.read())
-        # this replaces the large code block below for our purposes
         outpath_components = get_file_base_and_extension(bundle_path)
         minified_path = '{0}.min{1}'.format(outpath_components[0], outpath_components[1])
-        local('java -jar {yuicompressor} -v -o "{out_file}" --charset utf-8 "{in_file}"'.format(
-                yuicompressor=env.config.paths.yuicompressor_path, out_file=minified_path, in_file=bundle_path),
+        local('java -jar {paths.yuicompressor} -v -o "{out_file}" --charset utf-8 "{in_file}"'.format(
+                  out_file=minified_path, in_file=bundle_path, **env.config),
               capture=False)
 
 
-@task#('app-servers')
-def deploy():
+@task
+def deploy(dest):
+    dests = {
+        'prod': _deploy_prod,
+        'test': _deploy_test
+    }
+    dests[dest]() # will fail if wrong destination is given
+
+
+def _deploy_test():
+    # Deploy current branch
+    branch = local("git branch | grep '\*' | cut -d' ' -f2", capture=True)
+    local('git push {host_git_repo} {branch}'.format(branch=branch, **env.config))
+    with cd(env.config.paths.host_git):
+        run('export GIT_WORK_TREE={paths.test_git}; git checkout {branch} -f'.format(branch=branch, **env.config))
+    # Update environment
+    with cd(env.config.paths.test_git):
+        if (files.exists(env.config.paths.test_env)):
+            _msg('updating environment')
+            run('. {paths.test_env}/bin/activate && pip install -r envreq.txt'.format(**env.config))
+        else:
+            _msg('creating environment')
+            run('./rebuild_env.sh {paths.test_env}'.format(**env.config))
+    # Generate and upload settings file
+    build_settings('test')
+    _msg('uploading settings file')
+    local('scp {settings_dir}/settings_test.py {host}:{paths.test_project}/settings.py'.format(
+              settings_dir=env.settings_dir, **env.config))
+    # Import prod db
+    #_msg('importing db')
+    #_backup_db()
+    # Sync media
+    # Restart apache
+    #restart('test')
+    _msg('Run the recollect_static command to update static files.')
+
+
+def _deploy_prod():
+    print red('not implemented!')
+    return
     _msg('generating dummy config-file for the public')
     # hack
     _msg('updating release ID and timestamp')
@@ -102,44 +142,67 @@ def _deploy():
     _msg('uploading source tarball')
     local('scp ../{src_file} {user}@{host_string}:{media}/src/'.format(**env))
     _msg('syncing source')
-    local('rsync -avuz --delete --exclude="*.pyc" --exclude="/settings.py" --exclude="migrations/" {project}/ {user}@{host_string}:{path}/{project}/{project}/'.format(**env), capture=False)
+    local('rsync -avuz --delete --exclude="*.pyc" --exclude="/settings.py" --exclude="migrations/" {project}/ {user}@{host_string}:{base}/{project}/{project}/'.format(**env), capture=False)
     _msg('syncing remote settings')
-    local('rsync -avuz settings/settings-{host_string}.py {user}@{host_string}:{path}/{project}/{project}/settings.py'.format(**env))
+    local('rsync -avuz settings/settings-{host_string}.py {user}@{host_string}:{base}/{project}/{project}/settings.py'.format(**env))
     _msg('syncing static')
     local('rsync -avuz --delete {static_relative_dir}/ {user}@{host_string}:{static}/'.format(**env), capture=False)
 
 
-@task#('app-servers')
-def restart():
-    env().multirun(_restart)
-
-
-def _restart():
+@task
+def restart(dest):
+    dests = {
+        'prod': env.config.paths.restart,
+        'test': env.config.paths.test_restart
+    }
     _msg('restarting server')
-    res = run(env.restart.format(**env))
-    print res.stderr
+    run(dests[dest])
 
 
 @task
-def recollect_static():
+def stop_test():
+    _msg('stopping test server')
+    run(env.config.paths.test_stop)
+
+
+@task
+def recollect_static(dest='local'):
+    # TODO: for test (and prod) we should probably force build_static instead
+    dests = {
+        'local': _recollect_static_local,
+        'test': _recollect_static_prod
+    }
+    dests[dest]()
+
+
+def _recollect_static_local():
     with lcd(env.config.static_relative_dir):
         _msg('removing all files in static')
         local('rm -rf *')
     _msg('collecting static')
-    #print red('>>> warning: remember that things will go wrong if local and server environment is not the same!')
     local('echo yes | env python manage.py collectstatic', capture=False)
 
 
-@task#('app-servers')
-def remote_collect_static():
-    env().multirun(_remote_collect_static)
-
-
-def _remote_collect_static():
+@task
+def _recollect_static_test():
+    print red('not implemented!')
+    return
     _msg('collecting static')
     with cd(env.path.format(**env)):
         res = run('. {pyenv}/bin/activate; echo yes | python {project}/manage.py collectstatic')
         print res.stdout
+
+
+@task
+def backup():
+    #run db backup, media backup and download
+    print red('not implemented!')
+
+
+def _backup_db():
+    with cd(env.config.paths.git):
+        #run('. {paths.env}/bin/activate && ./manage.py dbdump 
+        pass
 
 
 @task
@@ -154,17 +217,36 @@ def build_translation():
     management.call_command('compilemessages')
 
 
-@task#('app-servers')
-def build_settings():
-    env().multirun(_build_settings)
+@task
+def build_settings(dest=False):
+    if (dest):
+        _build_settings(dest)
+    else:
+        _build_settings('prod')
+        _build_settings('test')
 
 
-def _build_settings():
-    _msg('building settings file')
-    local('./simple-proc-tpl.sh {settings_tpl} {private_data_dir}/settings_prod {settings_dir}/settings-{host_string}.py'.format(**env))
+def _build_settings(dest):
+    _msg('building settings file for {dest}'.format(dest=dest))
+    local('./simple-proc-tpl.sh {settings_tpl} {private_data_dir}/settings_{dest} {settings_dir}/settings_{dest}.py'.format(dest=dest, **env))
 
 
 @task
-def rebuild_fab_conf():
-    _msg('rebuilding {config_file}'.format(**env))
+def build_fabconfig():
+    _msg('building {config_file}'.format(**env))
     _build_fab_conf()
+
+
+@task
+def build_requirements():
+    _msg('building requirements file, excluding developer entries')
+    local('pip freeze > envreq.txt.tmp && combine envreq.txt.tmp xor envreq-dev.txt > envreq.txt && rm -f envreq.txt.tmp')
+
+
+@task
+def reset_comic_index():
+    print red('not implemented')
+#SELECT setval('comics_comic_id_seq', max(id)) FROM comics_comic;
+#SELECT setval('comics_comic_translation_id_seq', max(id)) FROM comics_comic_translation;
+#SELECT setval('comics_normalcomic_id_seq', max(id)) FROM comics_normalcomic;
+#SELECT setval('comics_normalcomic_translation_id_seq', max(id)) FROM comics_normalcomic_translation;
