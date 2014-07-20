114d354fe39638fa72ace2dccd6914641bf8ec3a
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2014-07-20 20:01:21 +0200

    More meaningful variable naming.

--- simple-proc-tpl.sh
+++ simple-proc-tpl.sh
@@ -3,11 +3,11 @@
 if [[ $# -lt 2 || $# -gt 3 ]]; then
     cat <<EOF >&2
 Simple Template Processor
-Usage: $0 <template> <data> [output]
+Usage: $0 <template> <context> [output]
 
 All arguments are filenames.
 Template field are marked as %FIELD%
-Data file should contain lines of the form
+Context file should contain lines of the form
 FIELD=value
 Field names must consist of uppercase letters and underscores only.
 Make a directive in the data file by beginning a line with %:
@@ -18,19 +18,19 @@ EOF
 fi
 
 TPL="$1"
-MAIN_DATA_FILE="$2"
-DATA_DIR=$(dirname "$MAIN_DATA_FILE")
+MAIN_CTX_FILE="$2"
+CTX_DIR=$(dirname "$MAIN_DATA_FILE")
 OUTPUT=${3:-'/dev/stdout'}
 
 process_data_file() {
-    DATA_FILE=$1
-    cat "$DATA_FILE" | while read LINE; do
+    CTX_FILE=$1
+    cat "$CTX_FILE" | while read LINE; do
         if echo "$LINE" | grep -q '^%'; then
             DIRECTIVE=$(echo "$LINE" | cut -d' ' -f1 | cut -c 2-)
             case $DIRECTIVE in
             'include')
                 INC_FILE=$(echo "$LINE" | cut -d' ' -f2)
-                process_data_file "$DATA_DIR/$INC_FILE"
+                process_data_file "$CTX_DIR/$INC_FILE"
                 if [ $? -ne 0 ]; then
                     echo "Error reading include file!" >&2
                     exit 1
@@ -46,6 +46,6 @@ process_data_file() {
     done
 }
 
-DATA=$(process_data_file "$MAIN_DATA_FILE")
+CTX=$(process_data_file "$MAIN_DATA_FILE")
 
-cat "$TPL" | sed "$(echo "$DATA" | sed -r 's/[\/&]/\\\0/g; s/([A-Z_]+)=(.*)/s\/%\1%\/\2\/g/')" > "$OUTPUT"
+cat "$TPL" | sed "$(echo "$CTX" | sed -r 's/[\/&]/\\\0/g; s/([A-Z_]+)=(.*)/s\/%\1%\/\2\/g/')" > "$OUTPUT"
