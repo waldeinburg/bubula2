0b34286351cd9b703f1ff917eabf9e4f1bedb4cd
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2014-11-16 21:13:45 +0100

    Adding model for intro to comics.

--- deploy_hooks.py
+++ deploy_hooks.py
@@ -3,5 +3,6 @@ from fabfile import _env
 
 def hook_syncdb__migrate(dest):
     with cd(env.config.paths[dest].git), _env(dest):
-        run('./manage.py migrate comics 0001 --fake')
+        run('./manage.py migrate comics 0001 --fake') # Use migrations for app
+        run('./manage.py migrate comics') # Rest of migrations
 
