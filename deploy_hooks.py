8cbcb2c23c419eee368e24c0a66603b915c45ae7
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2014-12-15 22:27:12 +0100

    Removed redundant command from deploy hook.

--- deploy_hooks.py
+++ deploy_hooks.py
@@ -5,5 +5,4 @@ def hook_syncdb__migrate(dest):
     cmd = local if dest == 'local' else run
     with cd(env.config.paths[dest].git), _env(dest):
         cmd('./manage.py migrate comics 0001 --fake') # Use migrations for app
-        cmd('./manage.py migrate comics') # Rest of migrations
 
