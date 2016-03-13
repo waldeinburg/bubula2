6df6798c9de99493e0fd43ad378b63ebadd7c1b6
Daniel Lundsgaard Skovenborg <waldeinburg@gmail.com>
2016-03-13 22:26:31 +0100

    Bundle and minify JS and CSS at build.

--- build.pl
+++ build.pl
@@ -1,51 +1,100 @@
-#!/usr/bin/env perl
+#!/usr/bin/perl
 
 use strict;
+use warnings;
 
+use Getopt::Long;
 use File::Find;
+use File::Basename;
+
+use constant YUICOMPRESSOR => '/usr/local/share/yuicompressor-2.4.8.jar';
 
 my %bundle_and_minify = (
-    'js/pack.js' => (
+    'js/pack.js' => [
         'js/main.js'
-    ),
-    'pack.css' => (
+    ],
+    'css/pack.css' => [
         'css/normalize.css',
         'css/main.css',
         'css/ir-sprites.css'
-    ),
-    'js/comic-pack.js' => (
+    ],
+    'js/comic-pack.js' => [
         'js/dropdown.js',
-        'js/single.js'
-    ),
-    'css/comic-pack.css' => (
+        'js/comic.js'
+    ],
+    'css/comic-pack.css' => [
         'css/dropdown.css',
-        'css/single.css',
-        'css/single-ir-sprites.css'
-    )
+        'css/comic.css',
+        'css/comic-ir-sprites.css'
+    ]
 );
 
-print "Building ...\n";
 
-$ENV{'JEKYLL_ENV'} = 'production';
-system('bundle', 'exec', 'jekyll', 'build');
+my $nobuild = 0;
+my $nominify = 0;
+my $nobundle = 0;
+my $keeporigs = 0;
+
+GetOptions(
+    'nobuild' => \$nobuild,
+    'nominify' => \$nominify,
+    'nobundle' => \$nobundle,
+    'keeporigs' => \$keeporigs
+) || exit;
 
 
-print "Minifying markup ...\n";
+unless ($nobuild) {
+    print "Building ...\n";
 
-sub wanted {
-    return unless /\.(html|xml|json)$/;
-    local $/; # undef record separator: slurp mode
-    my $f = $_;
-    # Open and slurp.
-    open(F, "<", $f) || die("$f: $!"); # chdir is on
-    $_ = <F>;
-    close(F);
-    # Edit and overwrite.
-    s/[ \n]+/ /g; # Replace multiple whitespace, including newline, with one space.
-    s/[ ]+$//; # Remove trailing whitespace.
-    open(F, ">", $f) || die("$f: $!");
-    print F;
-    close(F);
+    $ENV{'JEKYLL_ENV'} = 'production';
+    system('bundle', 'exec', 'jekyll', 'build');
 }
 
-find(\&wanted, '_site');
+unless ($nominify) {
+    print "Minifying markup ...\n";
+
+    sub wanted {
+        return unless /\.(html|xml|json)$/;
+        local $/; # undef record separator: slurp mode
+        my $f = $_;
+        # Open and slurp.
+        open(F, '<', $f) || die("$File::Find::name: $!"); # chdir is on
+        $_ = <F>;
+        close(F);
+        # Edit and overwrite.
+        s/[ \n]+/ /g; # Replace multiple whitespace, including newline, with one space.
+        s/[ ]+$//; # Remove trailing whitespace.
+        open(F, '>', $f) || die("$File::Find::name: $!");
+        print F;
+        close(F);
+    }
+
+    find(\&wanted, '_site');
+}
+
+unless ($nobundle) {
+    print "Bundling and minifying JS and CSS ...\n";
+
+    local $/;
+    chdir('_site');
+
+    for my $pack (keys %bundle_and_minify) {
+        my($name, $dir, $suffix) = fileparse($pack, qr/\.[^.]+/);
+        my $minpack = "$dir$name.min$suffix";
+
+        open(P, '>', $pack) || die("$pack: $!");
+        for my $f (@{$bundle_and_minify{$pack}}) {
+            open(F, '<', $f) || die("$f: $!");
+            $_ = <F>;
+            print P;
+            close(F);
+            unlink($f) unless $keeporigs;
+        }
+        close(P);
+
+        system('java', '-jar', YUICOMPRESSOR, '-v', '-o', $minpack, '--charset', 'utf-8', $pack);
+        # Return value always seems to indicate fail
+        #     || die("Failed execute: $!");
+        unlink($pack) unless $keeporigs;
+    }
+}
